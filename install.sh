0fef3b65fa888c9f2fbaad347d5f0ae1e9662b64
Felipe Rios <felipe.rios@vizir.com.br>
2021-02-05 20:59:21 -0300

    chore(dependencies): update all dependencies
    Signed-off-by: Felipe Rios <felipe.rios@vizir.com.br>

--- install.sh
+++ install.sh
@@ -12,17 +12,18 @@ curl -sL https://deb.nodesource.com/setup_12.x | bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
-apt-get update
-apt-get install -y \
+# install required
+apt-get update && apt-get install -y \
   dumb-init \
-  git \
+  vim git \
   nginx \
-  nodejs \
-  python \
-  python-pip \
+  nodejs yarn \
+  python python-pip \
   supervisor \
-  vim \
-  yarn
+
+# update all packages
+apt-get update && apt-get upgrade -y
+npm i npm@latest -g
 
 pip install --no-cache-dir awscli
 
